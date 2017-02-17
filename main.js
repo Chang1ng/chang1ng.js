088d0a091dfe452212395c98d213e805b5726aa8
Chang1ng <paxster@foxmail.com>
2017-02-17 14:23:01 +0800

    add
    add

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var util;
 util = {
 	//get elements by class
-	getByClass : function(oParent, sClass){
+	getByClass : function (oParent, sClass){
 		var aEle=oParent.getElementsByTagName('*');
 		var aResult=[];
 		var re=new RegExp('\\b'+sClass+'\\b', 'i');
@@ -21,13 +21,28 @@ util = {
 	*type-event type
 	*fn  -callback function
 	*/
-	addEvent(obj,type,fn){
-	    if(typeof obj.addEventListener!=='undefined'){
+	addEvent : function (obj,type,fn){
+	    if(typeof obj.addEventListener !== 'undefined'){
 	        obj.addEventListener(type,fn,false);
-	    }else if(typeof obj.attachEvent!=='undefined'){
+	    }else if(typeof obj.attachEvent !== 'undefined'){
 	        obj.attachEvent('on'+type,fn);
 	        fn.call(obj,window.event);
 	    }
-	    
-	}
+	},
+	removeEvent : function (obj,type,fn){
+	    if(typeof obj.removeEventListener!=='undefined'){
+	        obj.removeEventListener(type,fn,false);
+	    }else if(typeof obj.detachEvent !== 'undefined'){
+	        obj.detachEvent('on'+type);
+	    }
+	},
+	//close default event
+	preDef : function (event){
+    var e = event;
+    if(typeof e.preventDefault !== 'undefined'){
+        e.preventDefault();
+    }else{
+        e.returnValue=false;
+    }
+}
 };
