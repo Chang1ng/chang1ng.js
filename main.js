624b339a6463a558b53e2751a301f81c2e4ac0cf
Chang1ng <paxster@foxmail.com>
2017-02-17 14:06:29 +0800

    add
    add

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
-	 var aResult=[];
-	 var re=new RegExp('\\b'+sClass+'\\b', 'i');
-	 var i=0;
-	 
-	 for(i=0;i<aEle.length;i++){
-	  
-		 if(re.test(aEle[i].className)){
-		 	aResult.push(aEle[i]);
-		 }
-	 }
-	 
-	 return aResult;
-}
+var util;
+util = {
+	//get elements by class
+	getByClass : function(oParent, sClass){
+		var aEle=oParent.getElementsByTagName('*');
+		var aResult=[];
+		var re=new RegExp('\\b'+sClass+'\\b', 'i');
+		var i=0;
+		 
+		for(i=0;i<aEle.length;i++){
+		  
+			 if(re.test(aEle[i].className)){
+			 	aResult.push(aEle[i]);
+			 }
+		}
+		return aResult;
+	},
+	//addevent
+	/*
+	*obj -element
+	*type-event type
+	*fn  -callback function
+	*/
+	addEvent(obj,type,fn){
+	    if(typeof obj.addEventListener!=='undefined'){
+	        obj.addEventListener(type,fn,false);
+	    }else if(typeof obj.attachEvent!=='undefined'){
+	        obj.attachEvent('on'+type,fn);
+	        fn.call(obj,window.event);
+	    }
+	    
+	}
+};
